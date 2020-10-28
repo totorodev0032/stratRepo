d61c4b28f83c2d52b82af2aeb8db1c958ffeb85f
nagendra0032 <nagendrakumar0032@gmail.com>
2020-10-29 01:17:51 +0530

    login page added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,17 @@ const Login = () => {
     return (
         <div className = {styles.login_container}>
             <div className = {styles.login_box}>
-                
+                <div className = {styles.content_wrapper}>
+                    <div className = {styles.up}>
+                        <p> StratRepo </p>
+                    </div>
+                    <div className = {styles.down}>
+                        <input text = "type" placeholder =" Enter Your Email "  />
+                        <input text = "type" placeholder =" Password "  />
+                        <button>Login</button>
+                        <p>New here? Signup</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,10 +13,11 @@ const Navbar = () => {
                 <div className = {styles.nav_container}>
                     <h3>Home</h3>
                     <h3>About Us</h3>
-                    
+                    <Link to = '/login' style = {{textDecoration:'none'}}>
                     <div className = {styles.login_wrapper}>
                         <h3>Login</h3>
                     </div>
+                    </Link>
                     
                     
                   
